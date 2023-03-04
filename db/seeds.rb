348421d293838d7af89367afc5a443f74d227b9c
icika-max <icikakennedy@gmail.com>
2023-03-04 10:28:02 +0300

    edeting seed status

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,7 @@ User.all.each do |user|
       title: Faker::Lorem.sentence,
       description: Faker::Lorem.paragraph,
       due_date: Faker::Time.forward(days: 30),
-      status: [completed, incomplete].sample,
+      status: [true, false].sample,
       user_id: user.id
     )
   end
